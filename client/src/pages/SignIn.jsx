73411589524f705e2d32ad8574e671f86ffed3e0
Yaguera <ya-gotre@hotmail.com>
2024-01-05 18:11:50 -0300

    completed sign up page UI

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Signin() {
   return (
-    <div>Signin</div>
+    <div>
+      <h1>Sign In</h1>
+    </div>
   )
 }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className='max-w-lg p-3 mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <form className='flex flex-col gap-4'>
+        <input type="text" placeholder='Username' id='username' className='bg-slate-100 rounded-lg p-3' />
+        <input type="text" placeholder='Email' id='email' className='bg-slate-100 rounded-lg p-3' />
+        <input type="text" placeholder='Password' id='password' className='bg-slate-100 rounded-lg p-3' />
+        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign In</button>
+      </form>
+      <div className='flex gap-3'>
+        <p>Have an account?</p>
+        <Link to='/sign-in'>
+          <span className='text-blue-500'>Sign in</span>
+        </Link>
+      </div>
+    </div>
   )
 }
